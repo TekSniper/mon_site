d29e0cccb92c543163d5fd347799e9cc03b6a523
TekSniper <lubenzo2005@outlook.com>
2023-03-19 07:09:46 +0100

    Gallery & Session

--- mon_site/Program.cs
+++ mon_site/Program.cs
@@ -9,7 +9,7 @@ builder.Services.AddSession
 (
     o =>
     {
-        o.IdleTimeout = TimeSpan.FromMinutes(10);
+        o.IdleTimeout = TimeSpan.FromMinutes(30);
     }
     );
 
@@ -24,7 +24,6 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
-
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
